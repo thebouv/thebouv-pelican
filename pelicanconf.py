d4b1bd80e559ff42f60ce76a8f14e096752148f3
Anthony Bouvier <thebouv@gmail.com>
2021-03-16 15:11:35 -0500

    clean up with this bad commit message

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,8 +9,30 @@ TIMEZONE = 'America/Chicago'
 DEFAULT_LANG = 'en'
 
 # theme specific items -- begin
-THEME = 'theme/pelican-clean'
+THEME = 'theme/brutalist-theme'
 SITESUBTITLE = 'Geek of All Trades. Maker and Breaker. Tinkerer and Thinker-er.'
+SITEIMAGE = 'site-cover.jpg'
+DISPLAY_CATEGORIES_ON_MENU = False
+SITEDESCRIPTION = 'Geek of All Trades. Maker and Breaker. Tinkerer and Thinker-er.'
+FAVICON = 'thebouv.jpg'
+LOGO = 'thebouv.jpg'
+FIRST_NAME = 'Anthony'
+## google analytics (fake code commented out)
+GOOGLE_ANALYTICS = 'UA-55712845-1'
+## Twitter username for Twitter Card data
+TWITTER_USERNAME = '@thebouv'
+## Toggle display of theme attribution in the footer (scroll down and see)
+## Attribution is appreciated but totally fine to turn off!
+ATTRIBUTION = False
+## Add a link to the tags page to the menu
+## Other links can be added following the same tuple pattern 
+MENUITEMS = [('tags', '/tags')]
+## Social icons for footer
+## Set these to whatever your unique public URL is for that platform
+## I've left mine here as a example
+TWITTER = 'https://twitter.com/thebouv'
+GITHUB = 'https://github.com/thebouv'
+
 # theme specific items -- end
 
 # Feed generation is usually not desired when developing
