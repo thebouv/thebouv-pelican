21a79967103f721dff5e426169c9f2f6c1dff4ee
Anthony Bouvier <thebouv@gmail.com>
2021-04-14 20:50:44 -0500

    Clean up pelicanconf.py for new theme settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
+from datetime import date
+CURRENT_YEAR = date.today().year
 
 AUTHOR = 'Anthony Bouvier'
 SITENAME = 'thebouv'
@@ -9,24 +9,13 @@ TIMEZONE = 'America/Chicago'
 DEFAULT_LANG = 'en'
 
 # theme specific items -- begin
-THEME = 'theme/brutalist-theme'
+THEME = '/Users/thebouv/Documents/development/pelican-paper'
 SITEDESCRIPTION = 'Geek of All Trades. Maker and Breaker. Tinkerer and Thinker-er.'
 FAVICON = 'thebouv.jpg'
 LOGO = 'thebouv.jpg'
 DISPLAY_CATEGORIES_ON_MENU = False
-
-## Toggle display of theme attribution in the footer (scroll down and see)
-## Attribution is appreciated but totally fine to turn off!
-ATTRIBUTION = False
-## Add a link to the tags page to the menu
-## Other links can be added following the same tuple pattern 
-MENUITEMS = [('tags', '/tags')]
-## Social icons for footer
-## Set these to whatever your unique public URL is for that platform
-## I've left mine here as a example
 TWITTER = 'https://twitter.com/thebouv'
 GITHUB = 'https://github.com/thebouv'
-
 # theme specific items -- end
 
 # Feed generation is usually not desired when developing
@@ -35,17 +24,8 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = ()
-
-# Social widget
-SOCIAL = ()
-
 DEFAULT_PAGINATION = 10
 DELETE_OUTPUT_DIRECTORY = False
-
-# MARKDOWN = ['fenced_code', 'codehilite(css_class=highlight, guess_lang=False)']
 
 MARKDOWN = {
     'extension_configs': {
@@ -54,6 +34,3 @@ MARKDOWN = {
     },
     'output_format': 'html5',
 }
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
